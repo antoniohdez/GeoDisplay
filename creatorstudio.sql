b7545d5de0794d82424c72fe9bddcc831033706b
Antonio <antonio.hdez93@gmail.com>
2013-11-15 12:35:37 -0600

    Base de datos modificada para añadir logo

--- creatorstudio.sql
+++ creatorstudio.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 24, 2013 at 03:36 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Servidor: localhost
+-- Tiempo de generación: 15-11-2013 a las 18:35:00
+-- Versión del servidor: 5.1.44
+-- Versión de PHP: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,13 +16,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `creatorsstudio`
+-- Base de datos: `creatorstudio`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `points`
+-- Estructura de tabla para la tabla `points`
 --
 
 CREATE TABLE IF NOT EXISTS `points` (
@@ -38,28 +37,21 @@ CREATE TABLE IF NOT EXISTS `points` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `fk_points` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
--- Dumping data for table `points`
+-- Volcar la base de datos para la tabla `points`
 --
 
 INSERT INTO `points` (`user_id`, `point_name`, `description`, `latitude`, `longitude`, `image_path`, `url`, `text_url`, `id`) VALUES
-('creatorsstudio', 'Starbucks TEC', 'VisÃ­tenos ahora!', 20.735, -103.45550000000003, 'uploads/kang2.jpg', 'www.starbucks.com', 'Starbucks', 6),
-('creatorsstudio', 'Eros Facebook', 'Mi facebook', 20.735, -103.45550000000003, 'uploads/kang3.jpg', 'www.facebook,com', 'Facebook', 7),
-('creatorsstudio', 'Starbucks China', 'China', 39.85842879879298, 116.4017409375, 'uploads/AlienJ3.png', 'www.starbucks.com/china', 'China', 8);
+('creatorsstudio', 'Starbucks TEC', 'VisÃ­tenos ahora!', 29.7638521021792, -95.39153515625, 'uploads/circle.gif', 'www.starbucks.com', 'Starbucks', 6),
+('creatorsstudio', 'Eros Facebook', 'Mi facebook', 20.735, -103.4555, 'uploads/kang3.jpg', 'www.facebook,com', 'Facebook', 7),
+('creatorsstudio', 'Starbucks China', 'China', 39.858428798793, 116.4017409375, 'uploads/AlienJ3.png', 'www.starbucks.com/china', 'China', 8),
+('creatorsstudio', 'ldfjnvdl', 'fovfmgk', 20.7350903, -103.4550562, 'uploads/751E43AFE.png', 'www.creators.com', 'fjlnvf', 15);
 
 --
--- Triggers `points`
+-- (Evento) desencadenante `points`
 --
-DROP TRIGGER IF EXISTS `decreasePoint`;
-DELIMITER //
-CREATE TRIGGER `decreasePoint` AFTER DELETE ON `points`
- FOR EACH ROW UPDATE users 
-    SET users.points = users.points + 1
-    WHERE old.user_id = users.id
-//
-DELIMITER ;
 DROP TRIGGER IF EXISTS `increasePoint`;
 DELIMITER //
 CREATE TRIGGER `increasePoint` BEFORE INSERT ON `points`
@@ -68,11 +60,19 @@ CREATE TRIGGER `increasePoint` BEFORE INSERT ON `points`
     WHERE new.user_id = users.id
 //
 DELIMITER ;
+DROP TRIGGER IF EXISTS `decreasePoint`;
+DELIMITER //
+CREATE TRIGGER `decreasePoint` AFTER DELETE ON `points`
+ FOR EACH ROW UPDATE users 
+    SET users.points = users.points + 1
+    WHERE old.user_id = users.id
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -83,26 +83,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `points` int(11) NOT NULL,
   `country` varchar(128) NOT NULL,
   `city` varchar(128) NOT NULL,
+  `logo_path` varchar(512) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Volcar la base de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `points`, `country`, `city`) VALUES
-('creatorsstudio', 'Creators Studio', 'info@creatorsstudio.mx', '37f62f1363b04df4370753037853fe88', 1, 'México', 'Zapopan');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `points`, `country`, `city`, `logo_path`) VALUES
+('creatorsstudio', 'Creators Studio', 'info@creatorsstudio.mx', '37f62f1363b04df4370753037853fe88', 5, 'México', 'Zapopan', 'logo/751E43AFE.png');
 
 --
--- Constraints for dumped tables
+-- Filtros para las tablas descargadas (dump)
 --
 
 --
--- Constraints for table `points`
+-- Filtros para la tabla `points`
 --
 ALTER TABLE `points`
   ADD CONSTRAINT `fk_points` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
